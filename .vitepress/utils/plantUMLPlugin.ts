68b886233fc2dcba30448737a9dd35c9e474120b
DanXiaoMoYan <fs_dxmy@163.com>
2024-01-18 10:57:55 +0800

    ✨ Feature: 支持 PlantUML 的公用配置指令

--- .vitepress/utils/plantUMLPlugin.ts
+++ .vitepress/utils/plantUMLPlugin.ts
@@ -2,6 +2,7 @@ import * as MarkdownIt from 'markdown-it'
 import { encode } from 'plantuml-encoder'
 
 const server = 'https://www.plantuml.com/plantuml/svg'
+const skinParam = 'skinparam defaultFontName HarmonyOS Sans SC\n'
 
 /**
  * PlantUML 渲染插件
@@ -14,8 +15,15 @@ export function plantUMLPlugin(md: MarkdownIt): void {
 
     // 检查是否为 PlantUML 代码块
     if (token.info.trim() === 'plantuml') {
-      // 将原内容编码并返回渲染后的图片标签
-      const content = token.content.trim()
+      // 获取代码块内容
+      let content = token.content.trim()
+
+      // 设置主题参数
+      const startIndex = content.indexOf('@startuml')
+      const insertIndex = startIndex + '@startuml'.length + 1
+      content = content.slice(0, insertIndex) + skinParam + content.slice(insertIndex)
+
+      // 编码内容并返回渲染后的图片标签
       const encoded = encode(content)
       return `<p><img src="${server}/${encoded}" alt="PlantUML"></p>`
     }
